ff992b20094a0ff37c2ce0c1ea39f7a46564322b
Anannapath <152575328+Anannapath@users.noreply.github.com>
2025-03-12 15:32:51 +0700

    React API form-post request POST

--- server/index.js
+++ server/index.js
@@ -1,26 +1,25 @@
-///////////เปิด server  ///////////////
-const express = require('express')
-const app = express()
-const port = 8000
+const express=require('express')
+const app=express()
+const port=8000
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.static('public'))
 
-app.get('/',(request,response) => {
-    response.send(`
-    <!doctype html>
-    <html>
-    <head>
-    <title> Express.js </title>
-    </head>
-    <body>
-    <h3>Welcom to Express.js</h3>
-    <b>Express.js Fast, unpinionated, minimalist <br> 
-    web framwork for Node.js
-    </b>
-    </body>
-    </html>
-    `)
+app.post('/api/form-post',(request,response)=>{
+    let name=request.body.name||''
+    let email=request.body.email||''
+    let msg=request.body.message||''
+    let text=`
+        <table border='1'>
+            <caption>ข้อมูลที่ส่งขึ้นไป</caption>
+            <tr><td>ชื่อ:</td><td>${name}</td></tr>
+            <tr><td>อีเมล:</td><td>${email}</td></tr>
+            <tr><td>ข้อความ:</td><td>${msg}</td></tr>        
+        </table>
+    `
+    response.send(text)
 })
 
-app.listen(port, () => console.log('Sever listening on port' + port))
-///////////เปิด server http://localhost:8000/ //////////////
-
-/////////// รัน" http://localhost:8000/ " //////////////
+app.listen(port, ()=>{
+    console.log('Server listening on port'+port)
+})
